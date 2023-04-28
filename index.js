3bf31fb0ffaca301f880b2f15e14646dac4f406f
mayureshaher263 <mayuresh263aher@gmail.com>
2023-04-28 11:20:22 +0530

    update

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-const port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App listening on port ' + port + '!');
 });
